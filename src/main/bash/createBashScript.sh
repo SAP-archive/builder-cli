1ba76d0311f80c40b3fc752a794cf9fb37f2a88e
Pracht, Philipp <philipp.pracht@sap.com>
2015-08-31 11:49:50 +0200

    removed license parts

--- src/main/bash/createBashScript.sh
+++ src/main/bash/createBashScript.sh
@@ -8,13 +8,6 @@ extractedJar="/tmp/tmpcli-$secs.jar"
 echo "Creating script, jar file size: $jarsize"
 SCRIPT=$'#!/bin/bash
 
-# Copyright (c) 2000-2014 hybris AG
-# All rights reserved.
-#
-# This software is the confidential and proprietary information of hybris
-# ("Confidential Information"). You shall not disclose such Confidential
-# Information and shall use it only in accordance with the terms of the
-# license agreement you entered into with hybris.
 
 currentScript=${BASH_SOURCE[0]}
 tail -c '
