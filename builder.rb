7f36b5398472d38b651391f5e327697a4e59d248
Johannes Doberer <johannesdoberer@Johanness-MacBook-Pro.local>
2015-09-07 22:01:45 +0200

    changed url in builder.rb

--- builder.rb
+++ builder.rb
@@ -3,9 +3,9 @@ require 'formula'
 class Builder < Formula
   homepage 'https://github.com/hybris/builder-cli'
   head 'https://github.com/hybris/builder-cli'
-  url 'file:///usr/local/Library/Taps/hybris/homebrew-builder/builder-sdk-cli/builder.tgz'
+  url 'file:///usr/local/Library/Taps/hybris/builder-cli/homebrew-builder/builder.tgz'
   version '2.1.2'
-  sha1 '3306ccc9c200b1e88a0a1555a099bf300af477c3'
+  sha1 '303274fd60aec794c4451bdf354948f825a3b7d3'
     
   depends_on :arch => :x86_64
 
