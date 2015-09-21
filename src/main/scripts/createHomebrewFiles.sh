09c84c2ebfb78b12449aa8351fb6c583666406d4
JohannesDoberer <johannes.doberer@sap.com>
2015-09-21 14:10:22 +0200

    TSK-2898

--- src/main/scripts/createHomebrewFiles.sh
+++ src/main/scripts/createHomebrewFiles.sh
@@ -2,14 +2,14 @@
 echo 'Start homebrew action'
 
 echo 'create tmpDirectories and cp shell script to it'
-mkdir -p target/tmpBuilderDir/builder
-cp target/builder target/tmpBuilderDir/builder
+mkdir -p target/tmpBuilderDir
+cp target/builder target/tmpBuilderDir
 
 echo 'create tgz and copy to dist/homebrew-builder'
 cd target/tmpBuilderDir
-tar -cvzf builder/builder.tgz builder/builder
+tar -cvzf builder.tgz builder
 cd ../..
-cp target/tmpBuilderDir/builder/builder.tgz dist/homebrew-builder/builder.tgz
+cp target/tmpBuilderDir/builder.tgz dist/homebrew-builder/builder.tgz
 
 (shasum dist/homebrew-builder/builder.tgz | grep -Eo '^[^ ]+') > dist/homebrew-builder/builder.sha1
 
