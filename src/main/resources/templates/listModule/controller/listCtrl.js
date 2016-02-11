a59e4e897feb5ba9deb23cad8b78eafea0ab8914
JohannesDoberer <johannes.doberer@sap.com>
2016-02-11 16:47:32 +0100

    TSK-3447,list improvements

--- src/main/resources/templates/listModule/controller/listCtrl.js
+++ src/main/resources/templates/listModule/controller/listCtrl.js
@@ -1,8 +1,12 @@
-var defaultListApp = angular.module('defaultListApp', ["builder", "builder_editors","Restangular"]);
-defaultListApp.controller('defaultListCtrl', ['$scope', 'Restangular',function($scope,Restangular){
+var defaultListApp = angular.module('defaultListApp', ['builder', 'builder_editors']);
+defaultListApp.controller('defaultListCtrl', ['$scope','Restangular', function($scope, Restangular){
 
-    Restangular.setBaseUrl('${{SERVICE_URL}}');
-    Restangular.stripResponse(response);
+    /*Restangular.setBaseUrl('YOUR_SERVICE_URL');
+     Restangular.stripRestangular(response);
+     var headers={
+     'Authorization':'Bearer'+Builder.authManager().
+     }
+     $scope.items = Restangular.one('ENDPOINT').customGET("", headers);*/
 
     $scope.items = ['item1','item2','item3','item4'];
 }]);
