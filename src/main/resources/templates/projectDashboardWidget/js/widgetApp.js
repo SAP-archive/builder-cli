4950448f8de90acd337c843a111c448936c454ef
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2017-06-22 16:10:17 +0200

    TSK-5143

--- src/main/resources/templates/projectDashboardWidget/js/widgetApp.js
+++ src/main/resources/templates/projectDashboardWidget/js/widgetApp.js
@@ -1,31 +1,25 @@
-var widgetApp = angular.module('widgetApp', ['builderPlugin','restangular']);
+angular.module('widgetApp', ['builderPlugin']).
+    controller('WidgetCtrl', ['$scope','Restangular',function($scope, Restangular){
+        $scope.numberToDisplay;
+        BuilderPlugin.ready(function() {
+            Restangular
+                .setBaseUrl(BuilderPlugin.settings.serviceBasePath)
+                .withConfig(function(RestangularConfigurer) {
+                    RestangularConfigurer.setFullResponse(true);
+                })
+                .one(BuilderPlugin.settings.countableEndpoint+"?pageNumber=1&pageSize=1&totalCount=true")
+                .getList()
+                .then(function(response){
+                    $("#spinner").remove();
+                    $scope.numberToDisplay = response.headers("Hybris-Count");
+                },function(error){
+                    //handle error
+                }); 
+       });
 
-
-widgetApp.controller('WidgetController', ["$scope", "Restangular", function($scope, Restangular) {
-
-    $scope.project = {};
-
-    BuilderPlugin.ready(function() {
-        $scope.token = BuilderPlugin.authorizationData.accessToken;
-        $scope.scope = decodeURIComponent(BuilderPlugin.authorizationData.scope);
-        $scope.tenantId = BuilderPlugin.authorizationData.tenantId;
-        $scope.clientId = BuilderPlugin.authorizationData.clientId;
-
-        BuilderPlugin.sendEvent('slot1', BuilderPlugin.authorizationData.pluginId);
-        $scope.$apply();
-    });
-
-
-    $scope.updateProject = function(){
-       /* Restangular
-            .setErrorInterceptor(function (response) {
-                if (response.status == 403) {
-                    alert("FORBIDDEN");
-                }
-            })
-            .setBaseUrl('PATH_TO_YOUR_SERVICE_URL')
-            .one(PATH_TO_YOUR_ENDPOINT).customPUT($scope.project, "", {}, {}).then(function() {
-                BuilderPlugin.sendEvent('projectDescriptionChanged', $scope.project.description);
-            });*/
-    };
-}]);
+       $scope.goToSomewhere = function(){
+           var link = window.parent.Builder.linkManager().currentProject();
+           link.path(BuilderPlugin.settings.bottomLink);
+           link.open(true);
+       }
+    }]);
