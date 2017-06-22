4490bbb7a4a9e5ed01a5ad0a3757fefac1ee2998
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2017-06-22 14:13:14 +0200

    TSK-5143

---
+++ src/main/resources/templates/projectDashboardWidget/js/widgetApp.js
@@ -0,0 +1,31 @@
+var widgetApp = angular.module('widgetApp', ['builderPlugin','restangular']);
+
+
+widgetApp.controller('WidgetController', ["$scope", "Restangular", function($scope, Restangular) {
+
+    $scope.project = {};
+
+    BuilderPlugin.ready(function() {
+        $scope.token = BuilderPlugin.authorizationData.accessToken;
+        $scope.scope = decodeURIComponent(BuilderPlugin.authorizationData.scope);
+        $scope.tenantId = BuilderPlugin.authorizationData.tenantId;
+        $scope.clientId = BuilderPlugin.authorizationData.clientId;
+
+        BuilderPlugin.sendEvent('slot1', BuilderPlugin.authorizationData.pluginId);
+        $scope.$apply();
+    });
+
+
+    $scope.updateProject = function(){
+       /* Restangular
+            .setErrorInterceptor(function (response) {
+                if (response.status == 403) {
+                    alert("FORBIDDEN");
+                }
+            })
+            .setBaseUrl('PATH_TO_YOUR_SERVICE_URL')
+            .one(PATH_TO_YOUR_ENDPOINT).customPUT($scope.project, "", {}, {}).then(function() {
+                BuilderPlugin.sendEvent('projectDescriptionChanged', $scope.project.description);
+            });*/
+    };
+}]);
